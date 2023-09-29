6263b68e8a3317bd4a21b8378773ac88b09204f2
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-09-28 20:44:44 -0700

    Add samle get and put requests

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,11 +1,21 @@
 import express  from 'express';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (request,response) => {
-    response.send('Hello');
-})
-
-app.listen(8000, () => {
-    console.log("Server is listening on port 8000");
+    console.log(request.body);
+    response.send(`GET - Hello, ${request.body.name}`);
+    
 });
+
+app.post('/hello', (request,response) => {
+    console.log(request.body);
+    response.send(`POST - Hello, ${request.body.name}`);
+});
+
+
+
+app.listen(8001, () => {
+    console.log("Server is listening on port 8001");
+}); 
