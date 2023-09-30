014075f82b61551dc02ca18fc5480401d1a0f69f
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-09-29 18:33:12 -0700

    Add endpoint for comments

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -6,37 +6,84 @@ import express  from 'express';
 // fake database
 let articlesInfo = [{
     name: 'learn-react',
-    upvotes: 0
+    upvotes: 0,
+    comments: [],
 },
 {
     name: 'learn-node',
-    upvotes: 0
+    upvotes: 0,
+    comments: [],
+
 },
 {
     name: 'mongo-db',
-    upvotes: 0
+    upvotes: 0,
+    comments: [],
 }
 ]
 
 const app = express();
 app.use(express.json());
 
+/********************************************
+ * 
+ * Endpoint for upvote
+ * 
+ ********************************************/
+
 
 app.put('/api/articles/:name/upvote', (request, response) => {
     const { name } = request.params
-    // const name = request.params.name
+    
     const article = articlesInfo.find(
-        // a => a.name === articlesInfo[name]
-        a => a.name === name);
+        
+        articleSearch => articleSearch.name === name);
+
         if (article) {
             article.upvotes++;   
             response.send(`The ${name} has ${article.upvotes} upvotes`);
         }
         else
         {
-            response.send(`That article doesn\'t' exist`);
+            response.send(`Sorry dude. That article doesn\'t' exist.`);
+
+            
         }
 });
+
+/********************************************
+ * 
+ * Endpoint for adding a comment
+ * 
+ ********************************************/
+
+app.post('/api/articles/:name/comments', (request, response) => {
+    const { name } = request.params;
+    const { postedBy, text } = request.body
+    
+    const article = articlesInfo.find(
+    
+        articleSearch => articleSearch.name === name);
+
+        if (article) {
+            
+            article.comments.push({postedBy,text})
+            response.send(article.comments)
+            console.log(postedBy);
+            console.log(text);
+
+            // response.send(`Posted by: ${request.body.postedBy} and text: ${request.body.text}`)
+
+            
+        }
+        else
+        {
+            response.send(`Sorry. Comment not added`);
+
+            
+        }
+});
+
 
 app.listen(8001, () => {
     console.log("Server is listening on port 8001");
