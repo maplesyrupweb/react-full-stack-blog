115153ba0ca7d9b47f52b7168309c71c2c7f899f
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-10-02 22:57:23 -0700

    Use axios to load artice vote count

--- my-blog-frontend/src/pages/ArticlePage.js
+++ my-blog-frontend/src/pages/ArticlePage.js
@@ -2,26 +2,28 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import articles from "./article-content.js"
 import NotFoundPage from "./NotFoundPage.js";
+import axios from 'axios';
 
 const ArticlePage = () => {
 
     // initial state is 0 votes, no comments
     const [articleInfo, setArticleInfo] = useState({
-        upvotes: 0 , comments: [] 
-    })
+        upvotes: 0 , comments: []});
 
     const { articleId } = useParams();
 
     useEffect(() => {
-        setArticleInfo( {upvotes: Math.ceil(Math.random() * 10), comments: [] })
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+
+        loadArticleInfo();
     }, []);
 
-    
-    // const articleId = params.articleId;
-    // const { articleId } = params;
-
-
-    //find articl from articles array with matching article ID 
+        
+        
     const article = articles.find(article =>
         article.name === articleId);
  
@@ -30,23 +32,19 @@ const ArticlePage = () => {
             <NotFoundPage />
         )
         
-    } else {
-        //<></> is react fragment
-  
-    return (
+    }   
+      return (
         <>
-        <h1>{article.title}</h1>
-        <p>This article has {articleInfo.upvotes} upvote(s)</p>
-        {article.content.map((paragraph, index) => (
-            
-            <p key={index}>{paragraph}</p>
-        ))}
+            <h1>{article.title}</h1>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
+            {article.content.map((paragraph, index) => (
+                
+                <p key={index}>{paragraph}</p>
+            ))}
         </>        
 
     );
-    }
- 
-    
+
 }
 
 export default ArticlePage;
