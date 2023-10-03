f04e960854ad699c452afd9da08bfc4fddde027a
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-10-02 20:36:23 -0700

    Add useEffect and useState to article page

--- my-blog-frontend/src/pages/ArticlePage.js
+++ my-blog-frontend/src/pages/ArticlePage.js
@@ -1,9 +1,22 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import articles from "./article-content.js"
 import NotFoundPage from "./NotFoundPage.js";
 
 const ArticlePage = () => {
+
+    // initial state is 0 votes, no comments
+    const [articleInfo, setArticleInfo] = useState({
+        upvotes: 0 , comments: [] 
+    })
+
     const { articleId } = useParams();
+
+    useEffect(() => {
+        setArticleInfo( {upvotes: Math.ceil(Math.random() * 10), comments: [] })
+    }, []);
+
+    
     // const articleId = params.articleId;
     // const { articleId } = params;
 
@@ -17,15 +30,13 @@ const ArticlePage = () => {
             <NotFoundPage />
         )
         
-        
-    
     } else {
         //<></> is react fragment
   
     return (
         <>
         <h1>{article.title}</h1>
-
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
         {article.content.map((paragraph, index) => (
             
             <p key={index}>{paragraph}</p>
