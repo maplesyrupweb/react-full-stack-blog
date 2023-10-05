133cdd17ef4180643803538d3588c657963538ec
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-10-04 23:15:24 -0700

    Add login form

--- my-blog-frontend/src/pages/LoginPage.js
+++ my-blog-frontend/src/pages/LoginPage.js
@@ -1,9 +1,66 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
 const LoginPage = () => {
 
-    return(
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
-        <h1>Sign up</h1>
+    const navigate = useNavigate();
+    
+    const logIn = async () => {
+        try {
+            await signInWithEmailAndPassword(getAuth(), email, password);   
+            //route after successful login
+            navigate('/articles');
+        }
+        catch (error) {
+            setError(error.message);
+        }
+        
+    }
+    
+    return (
+    <>
+        <h1>Login Page</h1>
+
+        {error && <p className="error">{error}</p> }
+
+            <form>
+                <label>
+                    Email:
+                    <input 
+                        type="email" 
+                        placeholder='Email'
+                        id="email"
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
+
+                    />
+                </label>
+                
+                <label>
+                    Password:
+                    <input 
+                        type="password"
+                        placeholder='Password'
+                        id="password"
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                    />
+                </label>
+                
+
+            <button onClick={logIn}>Log In</button>
+
+            <Link to="/signup">Doesn't have an account, create one here</Link>
+                
+            </form>
+        </>
     )
+    
 
 }
 
--- my-blog-frontend/src/pages/SignUpPage.js
+++ my-blog-frontend/src/pages/SignUpPage.js
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+
 const SignUpPage = () => {
 
     return(
 
+        <>
         <h1>Create Account</h1>
+ 
+            <form>
+                <label>
+                    Email:
+                    <input type="email" />
+                </label>
+                
+                <label>
+                    Password:
+                    <input type="password" />
+                </label>
+                
+
+                <input type={"submit"}
+
+                    style={{ backgroundColor: "#a1eafb" }} />
+            </form>
+        </>
     )
 
 }
