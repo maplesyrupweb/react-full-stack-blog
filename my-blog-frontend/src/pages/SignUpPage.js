d7c3faf81aba07df5546e70d2ccd94a8841b8d7a
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-10-05 20:56:28 -0700

    Add the sign up functionality

--- my-blog-frontend/src/pages/SignUpPage.js
+++ my-blog-frontend/src/pages/SignUpPage.js
@@ -1,31 +1,72 @@
 import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
 
 const SignUpPage = () => {
 
-    return(
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [password2, setPassword2] = useState('');
+    const [error, setError] = useState('');
 
+    const navigate = useNavigate();
+    
+
+    const signUp = async () => {
+        try {
+            if (password !== password2) {
+
+                setError('Password do not match.');
+                console.log(`Passwords don't match: ${error}`)
+                return;
+                
+            }
+                console.log(`Password match ${password} and ${password2}`)
+                await createUserWithEmailAndPassword(getAuth(), email, password);
+                navigate('/articles');
+        }
+        catch (error) {
+            setError(error.message);
+            console.log(`Sign up error: ${error.message }`)
+        }
+    }
+
+    return (
         <>
-        <h1>Create Account</h1>
- 
-            <form>
-                <label>
-                    Email:
-                    <input type="email" />
-                </label>
+            <h1>Sign up</h1>
+    
+            {error && <p className="error">{error}</p> }
+    
+                <input 
+                    type="email" 
+                    placeholder='Email'
+                    id="email"
+                    value={email}
+                    onChange={event => setEmail(event.target.value)}
+                />
+                <input 
+                    type="password"
+                    placeholder='Password'
+                    id="password"
+                    value={password}
+                    onChange={event => setPassword(event.target.value)}
+                />
+
+                <input 
+                    type="password"
+                    placeholder='Confirm password'
+                    id="password2"
+                    value={password2}
+                    onChange={event => setPassword2(event.target.value)}
+                />
+    
+                <button onClick={signUp}>Sign Up</button>
+    
+                <Link to="/login">Log In</Link>                
                 
-                <label>
-                    Password:
-                    <input type="password" />
-                </label>
-                
-
-                <input type={"submit"}
-
-                    style={{ backgroundColor: "#a1eafb" }} />
-            </form>
-        </>
-    )
-
-}
+            </>
+        )
+    }
 
 export default SignUpPage;
